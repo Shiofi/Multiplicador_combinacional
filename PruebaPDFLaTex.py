192350ac7d70bc99be72b2614cb2dd5ead6ee6dc
Shiofi <fiorella7795@gmail.com>
2023-03-15 21:38:37 -0600

    Cambios en documento de prueba que genera un beamer

--- PruebaPDFLaTex.py
+++ PruebaPDFLaTex.py
@@ -2,21 +2,31 @@ import subprocess
 import os
 #import shutil
 
-# Crea un archivo con contenido LaTex
-latex_code = """\\documentclass{article}
+# Crea un archivo con contenido LaTex y en estilo beamer
+beamer_code = """\\documentclass{beamer}
 \\begin{document}
-Hola mundo!
+\\begin{frame}
+\\frametitle{Una prueba}
+    Este es un ejemplo de un documento al estilo beamer creado por LaTex desde Python.
+\\end{frame}
 \\end{document}
 """
 
 with open('documento.tex', 'w') as f: #'documento.tex' documento es el nombre del documento y .tex es el formato
-    f.write(latex_code)
+    f.write(beamer_code)
 
-# Compila el archivo LaTex en un archivo PDF
-subprocess.call(['pdflatex', '-interaction=nonstopmode', '-halt-on-error', 'documento.tex'])
-#subprocess.call(['pdflatex', 'documento.tex'])
+#Apartado donde el código solo genera un documento en formato PDF y otro en formato TeX
+# Compila el archivo LaTeX para generar el archivo auxiliar
+subprocess.call(['pdflatex', '-interaction=nonstopmode', '-halt-on-error', 'documento.tex', '-output-directory=output'])
+
+# Compila el archivo LaTeX por segunda vez para generar el archivo PDF final
+subprocess.call(['pdflatex', '-interaction=nonstopmode', '-halt-on-error', 'documento.tex', '-output-directory=output'])
+
+# Mueve el archivo PDF final a la carpeta raíz
+subprocess.call(['mv', 'output/documento.pdf', '.'])
 
 # Elimina los archivos intermedios generados por LaTeX
+subprocess.call(['rm', '-r', 'output'])
 for extension in ['aux', 'log', 'out']:
     os.remove(f'documento.{extension}')
 
